2cf413cf8fc80fecde87a9a657b60a20b1345cbb
suyeon <92326949+suyeonnnnnnn@users.noreply.github.com>
2023-07-13 01:23:55 +0900

    [Silver V] Title: 영화감독 숌, Time: 304 ms, Memory: 10008 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.js
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.js
@@ -1,8 +1,28 @@
-const input = Number(require('fs').readFileSync('/dev/stdin').toString());
-let count = 1;
-let num = 666;
-while (count !== input) {
-	num++;
-	if (num.toString().match(/666/g)) count++;
-}
-console.log(num);
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString();
+const N = Number(input);
+
+
+let ans, cnt, temp;
+
+  
+  ans = 0; // 영화 제목
+  cnt = 0; // 현재 몇번째 종말의 수인지
+
+  while (cnt !== N) {
+    ans++;
+    temp = ans;
+
+    // 수에 6이 적어도 3개 이상 들어가는지 판별
+    while (temp !== 0) {
+      if (temp % 1000 === 666) { // 종말의 숫자라면
+        cnt++;
+        break;
+      } else {
+        temp = Math.floor(temp / 10); // 일의 자리수 삭제
+      }
+    }
+  }
+
+  console.log(ans);
+  
