2b826a2433e2e60cddc54dfc90430c3e52e66cc9
suyeon <92326949+suyeonnnnnnn@users.noreply.github.com>
2023-07-13 01:26:19 +0900

    [Silver V] Title: 영화감독 숌, Time: 300 ms, Memory: 9992 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.js
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.js
@@ -9,13 +9,13 @@ let ans, cnt, temp;
   ans = 0; // 영화 제목
   cnt = 0; // 현재 몇번째 종말의 수인지
 
-  while (cnt !== N) {
+  while (cnt != N) {
     ans++;
     temp = ans;
 
     // 수에 6이 적어도 3개 이상 들어가는지 판별
-    while (temp !== 0) {
-      if (temp % 1000 === 666) { // 종말의 숫자라면
+    while (temp != 0) {
+      if (temp % 1000 == 666) { // 종말의 숫자라면
         cnt++;
         break;
       } else {
