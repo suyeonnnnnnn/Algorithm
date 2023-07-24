75ae2e37ff66f2ce9e7d24b2b083634e0e79b429
박수연 <92326949+suyeonnnnnnn@users.noreply.github.com>
2023-07-25 02:57:40 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 324 ms, Memory: 24600 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.js
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.js
@@ -1,24 +1,41 @@
 const fs = require('fs');
 const input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
 
-const t = Number(input[0]);
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    let count = 0;
 
-let inputIndex = 1; // Start index for reading test cases
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] < target) {
+            left = mid + 1;
+            count = mid + 1; // 배열에서 target보다 작은 요소의 개수를 저장
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return count;
+}
+
+const t = Number(input[0]);
+let inputIndex = 1;
 
 for (let i = 0; i < t; i++) {
-    const [a, b] = input[inputIndex++].split(' ').map(Number); // Parse a and b
-    const a_arr = input[inputIndex++].split(' ').map(Number); // Parse array A elements
-    const b_arr = input[inputIndex++].split(' ').map(Number); // Parse array B elements
+    const [n, m] = input[inputIndex++].split(' ').map(Number);
+    const a_arr = input[inputIndex++].split(' ').map(Number);
+    const b_arr = input[inputIndex++].split(' ').map(Number);
 
     a_arr.sort((a, b) => a - b);
     b_arr.sort((a, b) => a - b);
 
     let cnt = 0;
 
-    for (let k = 0; k < a; k++) {
-        for (let s = 0; s < b; s++) {
-            if (a_arr[k] > b_arr[s]) cnt++;
-        }
+    for (let k = 0; k < n; k++) {
+        // 배열 B에서 배열 A의 각 요소보다 작은 요소들의 개수를 세서 cnt에 더해줍니다.
+        cnt += binarySearch(b_arr, a_arr[k]);
     }
 
     console.log(cnt);
