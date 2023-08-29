5e087ce9b8f6fcf7631beeea1b2cdc20927ba3f7
suyeonnnnnnn <92326949+suyeonnnnnnn@users.noreply.github.com>
2023-08-30 02:42:53 +0900

    [level 2] Title: 양궁대회, Time: 14.62 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.js
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.js
@@ -1,40 +1,50 @@
+let bestResult = [];
+let maxDiff = -1;
+
+function calcScore(info, lion) {
+    let apeach = 0, lionScore = 0;
+
+    for (let i = 0; i < 11; i++) {
+        if (lion[i] > info[i]) {
+            lionScore += (10 - i);
+        } else if (info[i] > 0) {
+            apeach += (10 - i);
+        }
+    }
+    return lionScore - apeach;
+}
+
+function cmp(a, b) {
+    for (let i = 0; i < 11; i++) {
+        if (a[i] !== b[i]) {
+            return a[i] > b[i];
+        }
+    }
+    return false;
+}
+
+function backtracking(idx, arrowsLeft, lion, info) {
+    if (idx === 11) {
+        const diff = calcScore(info, lion);
+        if (diff <= 0) return;
+
+        if (diff > maxDiff || (diff === maxDiff && cmp(lion, bestResult))) {
+            bestResult = [...lion];
+            maxDiff = diff;
+        }
+        return;
+    }
+
+    for (let i = 0; i <= arrowsLeft; i++) {
+        lion[idx] = i > info[idx] ? i : info[idx] + 1;
+        backtracking(idx + 1, arrowsLeft - lion[idx], lion, info);
+    }
+}
+
 function solution(n, info) {
-    let ret = Array(11).fill(-1);
-    let arrow = Array(11).fill(0);
-    let maxScore = -Infinity;
+    const lion = Array(11).fill(0);
+    backtracking(0, n, lion, info);
 
-    function compare(a, b) {
-        for(let i = 10; i >= 0; i--) {
-            if(a[i] !== b[i]) return a[i] > b[i];
-        }
-        return false;
-    }
-
-    function bt(idx, left) {
-        if(idx === 10) {
-            arrow[idx] = left;
-            let score = 0;
-            for(let i = 0; i <= 10; i++) {
-                if(arrow[i] > info[i]) {
-                    score += 10-i;
-                } else if(info[i] > 0 && info[i] >= arrow[i]) {
-                    score -= 10-i;
-                }
-            }
-            if(score > 0 && (score > maxScore || (score === maxScore && compare(arrow, ret)))) {
-                maxScore = score;
-                ret = [...arrow];
-            }
-            return;
-        }
-        for(let i = 0; i <= left; i++) {
-            arrow[idx] = i;
-            bt(idx+1, left-i);
-        }
-    }
-
-    bt(0, n);
-
-    if(ret[0] === -1 || maxScore <= 0) return [-1];
-    return ret;
+    if (maxDiff === -1) return [-1];
+    return bestResult;
 }
