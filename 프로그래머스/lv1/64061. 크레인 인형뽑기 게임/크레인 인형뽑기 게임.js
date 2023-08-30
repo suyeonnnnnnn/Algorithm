95c08b41dd923c34ceb90380bc4adb95bb4362e9
suyeonnnnnnn <92326949+suyeonnnnnnn@users.noreply.github.com>
2023-08-31 01:13:18 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.38 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,18 +1,23 @@
 function solution(board, moves) {
     let answer = 0;
     let basket = [];
+    let real_board = [];
+
+    // Transpose the board
+    for(let i = 0; i < board.length; i++) {
+        real_board[i] = [];
+        for(let j = 0; j < board.length; j++) {
+            real_board[i][j] = board[j][i];
+        }
+    }
 
     for(let i = 0; i < moves.length; i++) {
         let column = moves[i] - 1;
         let pickedItem = null;
 
-        // 가장 위의 인형을 찾음.
-        for(let j = 0; j < board.length; j++) {
-            if(board[j][column] !== 0) {
-                pickedItem = board[j][column];
-                board[j][column] = 0; // 해당 위치의 인형을 제거
-                break;
-            }
+        // 전치행렬을 사용해서 가장 위에 있는 인형을 찾습니다.
+        while(real_board[column].length && !pickedItem) {
+            pickedItem = real_board[column].shift();
         }
 
         if(pickedItem) { // 인형이 선택된 경우
