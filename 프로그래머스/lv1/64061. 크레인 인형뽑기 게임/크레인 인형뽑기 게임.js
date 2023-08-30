fbc5d2ac0987ab76bdf328085c98f92defaa9791
suyeonnnnnnn <92326949+suyeonnnnnnn@users.noreply.github.com>
2023-08-31 01:51:52 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.93 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,31 +1,24 @@
 function solution(board, moves) {
     let answer = 0;
-    let basket = [];
-    let real_board = [];
+    let basket = []; // 인형을 담는 바구니
 
-    // Transpose the board
-    for(let i = 0; i < board.length; i++) {
-        real_board[i] = [];
-        for(let j = 0; j < board.length; j++) {
-            real_board[i][j] = board[j][i];
-        }
-    }
+    for (let i = 0; i < moves.length; i++) {
+        let column = moves[i] - 1; // 1-base indexing을 0-base indexing으로 변경
 
-    for(let i = 0; i < moves.length; i++) {
-        let column = moves[i] - 1;
-        let pickedItem = null;
+        for (let row = 0; row < board.length; row++) {
+            // 해당 위치에 인형이 있다면
+            if (board[row][column] !== 0) {
+                basket.push(board[row][column]); // 바구니에 인형을 넣는다.
+                board[row][column] = 0; // 그 위치의 인형을 제거한다.
 
-        // 전치행렬을 사용해서 가장 위에 있는 인형을 찾습니다.
-        while(real_board[column].length && !pickedItem) {
-            pickedItem = real_board[column].shift();
-        }
+                // 만약 바구니에 연속된 동일한 인형이 있다면 제거
+                if (basket.length >= 2 && basket[basket.length - 1] === basket[basket.length - 2]) {
+                    basket.pop();
+                    basket.pop();
+                    answer += 2; // 제거된 인형의 수를 더한다.
+                }
 
-        if(pickedItem) { // 인형이 선택된 경우
-            if(basket.length > 0 && basket[basket.length - 1] === pickedItem) {
-                basket.pop();
-                answer += 2; // 연속된 인형 두개가 제거되므로 2 증가
-            } else {
-                basket.push(pickedItem);
+                break; // 해당 column에서는 더 이상의 인형을 선택할 수 없으므로 loop를 탈출한다.
             }
         }
     }
