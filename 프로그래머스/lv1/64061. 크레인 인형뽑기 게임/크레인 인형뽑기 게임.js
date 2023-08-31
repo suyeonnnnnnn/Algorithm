5ce280ab2a4bf5fb1a72ff3a218d3c87c0290ade
suyeonnnnnnn <92326949+suyeonnnnnnn@users.noreply.github.com>
2023-08-31 20:45:08 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 5.64 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,24 +1,31 @@
 function solution(board, moves) {
     let answer = 0;
-    let basket = []; // 인형을 담는 바구니
+    let basket = [];
+    let real_board = [];
 
+    // 전치
+    for(let i = 0; i < board.length; i++) {
+        real_board[i] = [];
+        for(let j = 0; j < board.length; j++) {
+            real_board[i][j] = board[j][i];
+        }
+    }
+    console.log(real_board)
     for (let i = 0; i < moves.length; i++) {
-        let column = moves[i] - 1; // 1-base indexing을 0-base indexing으로 변경
 
-        for (let row = 0; row < board.length; row++) {
-            // 해당 위치에 인형이 있다면
-            if (board[row][column] !== 0) {
-                basket.push(board[row][column]); // 바구니에 인형을 넣는다.
-                board[row][column] = 0; // 그 위치의 인형을 제거한다.
+        while (real_board[moves[i] - 1].length !== 0) { // 해당 행이 비어있지 않으면
+            let pickedItem = real_board[moves[i] - 1].shift();
 
-                // 만약 바구니에 연속된 동일한 인형이 있다면 제거
+            if (pickedItem !== 0) {
+                basket.push(pickedItem);
+                
                 if (basket.length >= 2 && basket[basket.length - 1] === basket[basket.length - 2]) {
                     basket.pop();
                     basket.pop();
-                    answer += 2; // 제거된 인형의 수를 더한다.
+                    answer += 2;
                 }
-
-                break; // 해당 column에서는 더 이상의 인형을 선택할 수 없으므로 loop를 탈출한다.
+                break;
+                
             }
         }
     }
