5d7b56e6974ffa88385ba338c7c927ea5994d958
suyeonnnnnnn <92326949+suyeonnnnnnn@users.noreply.github.com>
2023-09-06 01:53:42 +0900

    [level 2] Title: 구명보트, Time: 27.75 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -0,0 +1,20 @@
+function solution(people, limit) {
+    let answer = 0;
+    
+    people.sort((a, b) => a - b);
+
+    let start = 0;
+    let end = people.length - 1;
+
+    while (start <= end) {
+        if (people[start] + people[end] <= limit) {
+            start++;
+            end--;
+        } else {
+            end--;  
+        }
+        answer++;
+    }
+
+    return answer;
+}
