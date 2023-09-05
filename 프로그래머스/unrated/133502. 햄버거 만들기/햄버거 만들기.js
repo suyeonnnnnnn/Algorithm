08e4630ede70834940fcbca3c0db148ab778c95a
suyeonnnnnnn <92326949+suyeonnnnnnn@users.noreply.github.com>
2023-09-05 18:06:22 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -3,12 +3,15 @@ function solution(ingredient) {
   
     for(let i = 0; i < ingredient.length; i++) {
         if(ingredient[i] == 1 && ingredient[i+1] == 2 && ingredient[i+2] == 3 && ingredient[i+3] == 1) {
-                ingredient.splice(i,4);
-                answer += 1;
-            i-=4;
+            ingredient.splice(i,4);
+            // console.log(ingredient);
+            answer += 1;
+            // i == 0; // 배열에서 4개의 원소를 제거했기 때문에 현재 위치를 4만큼 뒤로 이동
+            i-=4;  
         }
+        // console.log(i);
          
     }
-    console.log(ingredient)
+    // console.log(ingredient)
         return answer;
 }
