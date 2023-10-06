901a8b3b20eae19669a6c3fa777336b5c035deb5
suyeonnnnnnn <92326949+suyeonnnnnnn@users.noreply.github.com>
2023-10-06 12:12:42 +0900

    [level 2] Title: 주식가격, Time: 30.73 ms, Memory: 42 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.js
@@ -0,0 +1,15 @@
+function solution(prices) {
+    var answer = [];
+    
+    for(let i = 0; i < prices.length; i++) {
+        let count = 0;
+        for(let j = i+1; j < prices.length; j++){ 
+            count++;
+            if(prices[j] < prices[i]) {
+                break;
+            }
+        }
+        answer.push(count);
+    }
+    return answer; 
+}
