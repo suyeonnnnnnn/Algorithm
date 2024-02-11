0792d9f877216df085796e9583961f0041eaa101
suyeonnnnnnn <92326949+suyeonnnnnnn@users.noreply.github.com>
2024-02-12 01:58:02 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.49 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+    var answer = [];
+    let newNumbers = [...new Set(numbers)];
+
+    for(let i = 0; i < numbers.length; i++) {
+        for(let j = i+1; j < numbers.length; j++) {
+            answer.push(numbers[i] + numbers[j]);
+        }
+    }
+    answer = [...new Set(answer)];
+    answer.sort((a, b) => a-b);
+    return answer;
+}
